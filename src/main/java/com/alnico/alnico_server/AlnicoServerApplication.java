8f676695dd2f3111860ce3e4a5d6d3233f658a70
RecordOfJun <pritty0731@naver.com>
2023-05-04 22:58:38 +0900

    Initial commit

---
+++ src/main/java/com/alnico/alnico_server/AlnicoServerApplication.java
@@ -0,0 +1,13 @@
+package com.alnico.alnico_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlnicoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlnicoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnico/alnico_server/AlnicoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnico.alnico_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlnicoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
